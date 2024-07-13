f58d62b48a58545cddcdcd12cf62c96a1856e7ec
JustRainy <122139735+JustRainy@users.noreply.github.com>
2024-07-13 22:17:47 +0800

    a

--- test.py
+++ test.py
@@ -2,12 +2,22 @@ import pygame as PG
 
 
 class Human(PG.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, frames = 1):
         super().__init__()
-        self.image = PG.image.load("assets\\img\\character\\Human.png").convert()
-        self.rect = self.image.get_rect()
-        self.rect.x = 50
-        self.rect.y = 50
+        image = PG.image.load("assets\\img\\character\\Human.png").convert()
+        self.original_width = image.get_width() // frames  # 整除造型数，平均分图片
+        self.original_height = image.get_height()
+        self.images = []
+        frame_surface = PG.Surface([self.original_width, self.original_height])
+        x=0  # 初始坐标设置为0
+        for i in range(frames):
+            frame_surface = PG.Surface([self.original_width, self.original_height])
+            frame_surface.blit(image,[x,0])
+            self.images.append(frame_surface.copy())  # 注意这里需要用到Surface的copy方法才可以有效保存精灵矩形
+            x -= self.original_width    # 每一次减去一个造型的矩形固定宽度
+        self.image = self.images[0]  # 默认显示第一个造型
+        self.current_index = 0  # 设置默认造型标志
+        self.rect = self.image.get_rect()   # 初始化图片矩形
 
 PG.init()
 clock = PG.time.Clock()
@@ -27,7 +37,8 @@ while not done:
         if event.type == PG.QUIT:
             done = True
 
-    my_group.draw(screen)
     screen.fill((255, 255, 255))
+    my_group.draw(screen)
+    PG.display.flip()
 
     PG.display.update()
